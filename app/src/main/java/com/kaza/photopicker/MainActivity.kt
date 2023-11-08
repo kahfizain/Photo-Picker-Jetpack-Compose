5c0598eaa96f48fe2fb484e3c0d04099482ee027
KAHFI ZAIN <zkahfi@gmail.com>
2023-11-08 15:22:04 +0700

    push

--- app/src/main/java/com/kaza/photopicker/MainActivity.kt
+++ app/src/main/java/com/kaza/photopicker/MainActivity.kt
@@ -44,8 +44,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
